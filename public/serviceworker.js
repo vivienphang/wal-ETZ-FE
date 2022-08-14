1319b4d1e5d4fbf3dd2bee08cd312f9afd5c5a99
Luthor9269 <mayank_aswal@live.com>
2022-08-14 09:53:52 +0700

    Created PWA-REACT

---
+++ public/serviceworker.js
@@ -0,0 +1,48 @@
+//name of our cache storage
+const CACHA_NAME = "version-1";
+//main url when online and when offline
+const urlsToCache = ["index.html", "offline.html"];
+
+//this represents the service worker
+const self = this;
+//installation event to install service worker
+//service worker it(self)
+self.addEventListener("install", (event) => {
+  //open cache and add file to the cache
+  event.waitUntil(
+    //caches.open returns a promise
+    caches.open(CACHA_NAME).then((cache) => {
+      console.log("Opened Cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+//Listen for requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => {
+        return caches.match("offline.html");
+      });
+    })
+  );
+});
+
+//Activate Service Worker
+self.addEventListener("activate", (event) => {
+  const cacheWhiteList = [];
+  cacheWhiteList.push(CACHA_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhiteList.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
