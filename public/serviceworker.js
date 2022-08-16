cf17b693baaadddd7a1ff8c53d517138c355ce7c
Luthor9269 <mayank_aswal@live.com>
2022-08-16 17:12:38 +0700

    Fixed Offline Mode

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,54 +1,52 @@
 //name of our cache storage
-const CACHA_NAME = "v1";
-//main url when online and when offline
-const urlsToCache = ["index.html", "offline.html"];
+const cacheName = "v1";
 
-//this represents the service worker
-const self = this;
-//installation event to install service worker
-//service worker it(self)
-self.addEventListener("install", (event) => {
-  //open cache and add file to the cache
-  event.waitUntil(
-    //caches.open returns a promise
-    caches.open(CACHA_NAME).then((cache) => {
-      console.log("Opened Cache");
-      return cache.addAll(urlsToCache);
-    })
-  );
+//calling install event
+//self is this service worker
+// const self = this;
+
+self.addEventListener("install", (e) => {
+  console.log("Service Worker: Installed");
 });
 
-//Listen for requests
-self.addEventListener("fetch", (event) => {
-  if (!(event.request.url.indexOf("http") === 0)) return;
-  event.respondWith(
-    caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => {
-        return caches.match("offline.html");
-      });
-    })
-  );
-});
-
-//Activate Service Worker
-self.addEventListener("activate", (event) => {
-  const cacheWhiteList = [];
-  cacheWhiteList.push(CACHA_NAME);
-
-  event.waitUntil(
+//activate event
+self.addEventListener("activate", (e) => {
+  console.log("Service Worker: Activated");
+  //Remove unwanted caches here
+  e.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhiteList.includes(cacheName)) {
-            return caches.delete(cacheName);
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            console.log("Service Worker: Deleting Old Chache");
+            return caches.delete(cache);
           }
         })
       );
     })
   );
 });
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////Offline site caching////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// version of the current cahching folders
-// to be used when pushing new cahching
+
+//fetch files to display
+self.addEventListener("fetch", (e) => {
+  console.log("Service Worker: Fetching");
+  //check if live site is available if not load cached files
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        //Make a Copy of response
+        const resClone = res.clone();
+        ///openning a cache
+        caches.open(cacheName).then((cache) => {
+          //Adding the whole response files as a cache
+          cache.put(e.request, resClone);
+        });
+        return res;
+      })
+      .catch((err) => {
+        caches.match(e.request).then((res) => {
+          return res;
+        });
+      })
+  );
+});
