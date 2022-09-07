832e1f1a4c525cacf1240348cc9ebdee9b948a79
Vivien Phang <101629147+vivienphang@users.noreply.github.com>
2022-09-07 18:11:50 +0800

    wip

--- src/atoms/UserName.tsx
+++ src/atoms/UserName.tsx
@@ -26,77 +26,77 @@
 //   );
 // }
 
-// import React, { useContext } from "react";
+import React, { useContext } from "react";
 
-// import {
-//   ButtonGroup,
-//   Editable,
-//   EditableInput,
-//   EditablePreview,
-//   Flex,
-//   IconButton,
-//   Input,
-//   useEditableControls,
-// } from "@chakra-ui/react";
-// import { CheckIcon, CloseIcon, EditIcon } from "@chakra-ui/icons";
-// import { UserContext } from "../provider/GlobalProvider";
-// // import getData from "../reducers/globalAction";
+import {
+  ButtonGroup,
+  Editable,
+  EditableInput,
+  EditablePreview,
+  Flex,
+  IconButton,
+  Input,
+  useEditableControls,
+} from "@chakra-ui/react";
+import { CheckIcon, CloseIcon, EditIcon } from "@chakra-ui/icons";
+import { UserContext } from "../provider/GlobalProvider";
+// import getData from "../reducers/globalAction";
 
-// export default function ProfileForm() {
-//   const { userState } = useContext(UserContext);
+export default function ProfileForm() {
+  const { userState } = useContext(UserContext);
 
-//   function EditableControls() {
-//     const {
-//       isEditing,
-//       getSubmitButtonProps,
-//       getCancelButtonProps,
-//       getEditButtonProps,
-//     } = useEditableControls();
+  function EditableControls() {
+    const {
+      isEditing,
+      getSubmitButtonProps,
+      getCancelButtonProps,
+      getEditButtonProps,
+    } = useEditableControls();
 
-//     return isEditing ? (
-//       <ButtonGroup justifyContent="center" size="sm">
-//         <IconButton
-//           aria-label="check button"
-//           icon={<CheckIcon />}
-//           {...getSubmitButtonProps()}
-//         />
-//         <IconButton
-//           aria-label="close button"
-//           icon={<CloseIcon />}
-//           {...getCancelButtonProps()}
-//         />
-//       </ButtonGroup>
-//     ) : (
-//       <Flex justifyContent="center">
-//         <IconButton
-//           aria-label="edit button"
-//           size="sm"
-//           icon={<EditIcon />}
-//           {...getEditButtonProps()}
-//         />
-//       </Flex>
-//     );
-//   }
+    return isEditing ? (
+      <ButtonGroup justifyContent="center" size="sm">
+        <IconButton
+          aria-label="check button"
+          icon={<CheckIcon />}
+          {...getSubmitButtonProps()}
+        />
+        <IconButton
+          aria-label="close button"
+          icon={<CloseIcon />}
+          {...getCancelButtonProps()}
+        />
+      </ButtonGroup>
+    ) : (
+      <Flex justifyContent="center">
+        <IconButton
+          aria-label="edit button"
+          size="sm"
+          icon={<EditIcon />}
+          {...getEditButtonProps()}
+        />
+      </Flex>
+    );
+  }
 
-//   return (
-//     <div>
-//       <Editable
-//         textAlign="center"
-//         defaultValue={userState?.username}
-//         fontSize="2xl"
-//         isPreviewFocusable={false}
-//       >
-//         <EditablePreview />
-//         {/* Here is the custom input */}
-//         <Input variant="outline" value={userState?.username} />
-//         <EditableControls />
-//       </Editable>
-//       <div>
-//         <Editable value={userState?.username} shadow="md" borderRadius="10px">
-//           <EditablePreview />
-//           <EditableInput />
-//         </Editable>
-//       </div>
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <Editable
+        textAlign="center"
+        defaultValue={userState?.username}
+        fontSize="2xl"
+        isPreviewFocusable={false}
+      >
+        <EditablePreview />
+        {/* Here is the custom input */}
+        <Input variant="outline" value={userState?.username} />
+        <EditableControls />
+      </Editable>
+      <div>
+        <Editable value={userState?.username} shadow="md" borderRadius="10px">
+          <EditablePreview />
+          <EditableInput />
+        </Editable>
+      </div>
+    </div>
+  );
+}
