7497d724d8a69a0eb99a54a3a150ec3d756ed774
Vivien Phang <101629147+vivienphang@users.noreply.github.com>
2022-09-08 23:46:50 +0800

    added stuff in username

--- src/atoms/UserName.tsx
+++ src/atoms/UserName.tsx
@@ -1,59 +1,67 @@
-import React, { useContext } from "react";
-
+import React, { useContext, useState } from "react";
+import axios from "axios";
 import {
   Box,
   Button,
-  // ButtonGroup,
-  // Editable,
-  // EditableInput,
-  // EditablePreview,
-  // Flex,
   FormControl,
+  FormErrorMessage,
+  FormHelperText,
   FormLabel,
   // IconButton,
   Input,
   Switch,
-  // useEditableControls,
 } from "@chakra-ui/react";
-// import { CheckIcon, CloseIcon, EditIcon } from "@chakra-ui/icons";
+import { useNavigate } from "react-router-dom";
 import { UserContext } from "../provider/GlobalProvider";
+// import { CheckIcon, CloseIcon, EditIcon } from "@chakra-ui/icons";
+
+axios.defaults.withCredentials = true;
 
 export default function ProfileForm() {
   const { userState } = useContext(UserContext);
+  const navigate = useNavigate();
+  const [username, setUsername] = useState(userState?.username);
+  const [currency, setCurrency] = useState(userState?.defaultCurrency);
 
-  // function EditableControls() {
-  //   const {
-  //     isEditing,
-  //     getSubmitButtonProps,
-  //     getCancelButtonProps,
-  //     getEditButtonProps,
-  //   } = useEditableControls();
+  const handleUsernameChange: React.ChangeEventHandler<HTMLInputElement> = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => setUsername(event.target.value);
 
-  //   return isEditing ? (
-  //     <ButtonGroup justifyContent="center" size="sm">
-  //       <IconButton
-  //         aria-label="check button"
-  //         icon={<CheckIcon />}
-  //         {...getSubmitButtonProps()}
-  //       />
-  //       <IconButton
-  //         aria-label="close button"
-  //         icon={<CloseIcon />}
-  //         {...getCancelButtonProps()}
-  //       />
-  //     </ButtonGroup>
-  //   ) : (
-  //     <Flex justifyContent="center">
-  //       <IconButton
-  //         aria-label="edit button"
-  //         size="sm"
-  //         icon={<EditIcon />}
-  //         {...getEditButtonProps()}
-  //       />
-  //     </Flex>
-  //   );
-  // }
+  const handleCurrencyChange: React.ChangeEventHandler<HTMLInputElement> = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => setCurrency(event.target.value);
 
+  // Input error handlers
+  const usernameError = username === "";
+  const currencyError = currency === "";
+  // Update checks
+  // i. if username already exists in DB
+  // ii. regex conditions for username
+  // iii. findIdAndUpdate, returnDocument: "after"
+  const handleUpdateBtn: React.FormEventHandler<HTMLFormElement> = async (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
+    console.log("update button clicked");
+    const updateData = {
+      username,
+      currency,
+    };
+    if (username) {
+      try {
+        const id: any = userState?._id;
+        console.log("THIS IS ID:", id);
+        const updateUsername = await axios.post(
+          `${process.env.REACT_APP_BACKEND_URL}/users/updateProfile/${id}`,
+          updateData
+        );
+        console.log("update username", updateUsername);
+        navigate("/home");
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
   return (
     <div>
       <FormControl display="flex" alignItems="end">
@@ -65,15 +73,40 @@ export default function ProfileForm() {
       <br />
       <Box w="100%" p={3} borderWidth="2px" borderRadius="lg" bg="gray.100">
         <form>
-          <FormControl>
+          <FormControl isInvalid={usernameError}>
             <FormLabel>Username:</FormLabel>
-            <Input type="text" value={userState?.username} />
+            <Input
+              type="text"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+            {!usernameError ? (
+              <FormHelperText />
+            ) : (
+              <FormErrorMessage>Username is required.</FormErrorMessage>
+            )}
             <br />
+          </FormControl>
+          <FormControl isInvalid={currencyError}>
             <FormLabel>Default currency:</FormLabel>
-            <Input type="text" value={userState?.defaultCurrency} />
+            <Input
+              type="text"
+              value={currency}
+              onChange={handleCurrencyChange}
+            />
+            {!currencyError ? (
+              <FormHelperText />
+            ) : (
+              <FormErrorMessage>Default currency is required.</FormErrorMessage>
+            )}
           </FormControl>
           <br />
-          <Button display="flex" alignItems="center" colorScheme="teal">
+          <Button
+            display="flex"
+            alignItems="center"
+            colorScheme="teal"
+            onClick={handleUpdateBtn}
+          >
             Update
           </Button>
         </form>
--- src/components/Filter 2.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Filter() {
-  return (
-    <div>
-      <h1>Filter component</h1>
-    </div>
-  );
-}
