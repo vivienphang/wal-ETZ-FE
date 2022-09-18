de534d27c6dbd77d87c9e68b921a4057058c1ecd
floatingtales <58957425+floatingtales@users.noreply.github.com>
2022-09-18 16:23:24 +0700

    basic homepage layout

---
+++ src/constants/colorList.ts
@@ -0,0 +1,9 @@
+export default {
+  primary: "#FFFFEB",
+  secondary: "#AFD9FE",
+  component: "#B29995",
+  buttonPrimary: "#5DB3FD",
+  buttonSecondary: "#FF9673",
+  moneyGreen: "#48BB78",
+  moneyRed: "#F56565",
+};
